58ad3542a270762cff3f44212eae000dcb94aca7
kido <bouningenn@gmail.com>
2026-09-22 10:42:40 +0900

    docs: keep use scoped to each attempt

--- docs/examples/middleware.test.ts
+++ docs/examples/middleware.test.ts
@@ -2,7 +2,7 @@ import { Test } from 'hanamaru'
 import { createDatabase, countUsers } from './database.ts'
 
 export const userCount = new Test()
-  .use('perAttempt', async (_, next) => {
+  .use(async (_, next) => {
     const db = await createDatabase()
     try {
       return await next({ db, expected: 3 })
